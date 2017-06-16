f8ddf090235c99f93af1a995e94f03de784b0fdf
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-06-16 12:02:34 +0300

    Added another solution for Pr. 4

---
+++ 07.Dictionaries-Lambda-LINQ/07.Dictionaries-Lambda-LINQ-Exercises/04.FixEmails_2/FixEmails.cs
@@ -0,0 +1,39 @@
+﻿namespace _04.FixEmails_2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FixEmails
+    {
+        public static void Main()
+        {
+            var usersEmails = new Dictionary<string, string>();
+
+            string line = Console.ReadLine();
+
+            while (line != "stop")
+            {
+                string name = line;
+                string email = Console.ReadLine();
+
+                if (!usersEmails.ContainsKey(name))
+                {
+                    usersEmails[name] = string.Empty;
+                }
+
+                usersEmails[name] = email;
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var kvp in usersEmails
+                .Where(kvp => !(kvp.Value.EndsWith(".us") || kvp.Value.EndsWith(".uk"))))
+            {
+                string name = kvp.Key;
+                string emailAddress = kvp.Value;
+                Console.WriteLine($"{name} -> {emailAddress}");
+            }
+        }
+    }
+}
