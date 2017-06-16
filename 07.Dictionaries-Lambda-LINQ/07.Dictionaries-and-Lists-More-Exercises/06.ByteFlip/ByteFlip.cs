b3c8cb1271ecea96da065dde35c27deb6f729888
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-06-16 23:56:23 +0300

    Adding Solution of Pr. 06

---
+++ 07.Dictionaries-Lambda-LINQ/07.Dictionaries-and-Lists-More-Exercises/06.ByteFlip/ByteFlip.cs
@@ -0,0 +1,27 @@
+﻿namespace _06.ByteFlip
+{
+    using System;
+    using System.Linq;
+
+    public class ByteFlip
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Where(x => x.Length == 2).ToList();
+
+            var resultString = input
+                .Select(c => new string(c.Reverse().ToArray()))
+                .Reverse()
+                .ToList();
+
+            string ascii = string.Empty;
+
+            foreach (var item in resultString)
+            {
+                ascii += (char)(Convert.ToInt32(item, 16));
+            }
+
+            Console.WriteLine(ascii);
+        }
+    }
+}
