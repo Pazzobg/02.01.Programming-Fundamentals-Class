9f3b65503a7f127b39afd7802b97100e19f2206a
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-06-30 19:25:45 +0300

    Updated Solution of Pr.07. Now with result 100/100

--- 11.Regular-Expressions-Regex/12.Regular-Expressions-Regex-Exercises/07.QueryMess/QueryMess.cs
+++ 11.Regular-Expressions-Regex/12.Regular-Expressions-Regex-Exercises/07.QueryMess/QueryMess.cs
@@ -2,76 +2,69 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Text.RegularExpressions;
 
     public class QueryMess
     {
         public static void Main()
         {
-            string input = Console.ReadLine();
+            var outputDict = new Dictionary<string, List<string>>();
 
-            string pattern = @"([\w+%*\-\.()]+)=([\w+%*\-\.()]+)&?";
+            string pattern = @"([^&?]+)=([^&?]+)";
             var regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
 
             while (input != "END")
             {
-                var outputDict = new Dictionary<string, List<string>>();
 
                 var matches = regex.Matches(input);
 
                 foreach (Match match in matches)
                 {
 
-                    string key = match.Groups[1].Value.ToString();
-                    string value = match.Groups[2].Value.ToString();
+                    string currentKey = ExtractKey(match.Groups[1].Value.ToString());
+                    string currentValue = ExtractValue(match.Groups[2].Value.ToString());
 
-                    if (key.Contains("%20"))
+                    if (!outputDict.ContainsKey(currentKey))
                     {
-                        key = key.Remove(key.IndexOf("%20"), 3);
-                    }
-                    if (value.Contains("%20"))
-                    {
-                        value = value.Remove(value.IndexOf("%20"), 3);
-                    }
-                    if (key.Contains("+"))
-                    {
-                        key = key.Remove(key.IndexOf("+"), 1);
-                    }
-                    if (value.Contains("+"))
-                    {
-                        value = value.Remove(value.IndexOf("+"), 1);
-                    }
-                    if (key.ToLower() == "url" && value.ToLower().StartsWith("http"))
-                    {
-                        continue;
+                        outputDict[currentKey] = new List<string>();
                     }
 
-                    if (!outputDict.ContainsKey(key))
-                    {
-                        outputDict[key] = new List<string>();
-                    }
-
-                    outputDict[key].Add(value);
+                    outputDict[currentKey].Add(currentValue);
                 }
-
-                var sb = new StringBuilder();
 
                 foreach (var kvp in outputDict)
                 {
                     string key = kvp.Key;
                     var valuesList = kvp.Value;
 
-                    sb.Append(key + "=[");
-                    sb.Append(string.Join(", ", valuesList) + "]");
+                    Console.Write($"{key}=[{string.Join(", ", valuesList)}]");
                 }
 
-                Console.WriteLine(sb.ToString());
+                Console.WriteLine();
 
+                outputDict.Clear();
                 input = Console.ReadLine();
             }
+        }
+
+        public static string ExtractKey(string currentKeyString)
+        {
+            currentKeyString = Regex.Replace(currentKeyString, @"%20|\+", " ");
+            currentKeyString = Regex.Replace(currentKeyString, @"\s+", " ");
+            currentKeyString = currentKeyString.Trim();
+
+            return currentKeyString;
+        }
+
+        public static string ExtractValue(string currentValueString)
+        {
+            currentValueString = Regex.Replace(currentValueString, @"%20|\+", " ");
+            currentValueString = Regex.Replace(currentValueString, @"\s+", " ");
+            currentValueString = currentValueString.Trim();
+
+            return currentValueString;
         }
     }
 }
