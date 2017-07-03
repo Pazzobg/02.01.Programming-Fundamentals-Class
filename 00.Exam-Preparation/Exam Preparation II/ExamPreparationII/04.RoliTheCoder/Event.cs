9babae34dc3fd3edcff92ffe9e8e5c71e1a3da31
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-03 22:54:44 +0300

    Added Solution to Pr.04.

---
+++ 00.Exam-Preparation/Exam Preparation II/ExamPreparationII/04.RoliTheCoder/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace _04.RoliTheCoder
+{
+    using System.Collections.Generic;
+
+    public class Event
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public SortedSet<string> Participants { get; set; }
+    }
+}
---
+++ 00.Exam-Preparation/Exam Preparation II/ExamPreparationII/04.RoliTheCoder/RoliTheCoder.cs
@@ -0,0 +1,77 @@
+﻿namespace _04.RoliTheCoder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class RoliTheCoder
+    {
+        public static void Main()
+        {
+            var eventsAndParticipants = new SortedDictionary<string, Event>();
+
+            var eventPattern = @"^(.+?) #(\w+)( (@\w+))*";
+            var regex = new Regex(eventPattern);
+
+            string line = Console.ReadLine();
+
+            while (line != "Time for Code")
+            {
+                var match = regex.Match(line);
+                if (!match.Success)
+                {
+                    line = Console.ReadLine();
+                    continue;
+                }
+
+                string[] input = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string currentId = input[0];
+                string currentName = input[1].TrimStart('#');
+
+                var currentEvent = new Event
+                {
+                    ID = currentId,
+                    Name = currentName,
+                    Participants = new SortedSet<string>()
+                };
+
+                if (!eventsAndParticipants.ContainsKey(currentId))
+                {
+                    eventsAndParticipants[currentId] = currentEvent;
+                    AddParticipants(eventsAndParticipants, input, currentId);
+                }
+                else if (eventsAndParticipants.ContainsKey(currentId)
+                    && eventsAndParticipants[currentId].Name == currentName)
+                {
+                    AddParticipants(eventsAndParticipants, input, currentId);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var kvp in eventsAndParticipants
+                .OrderByDescending(x => x.Value.Participants.Count)
+                .ThenBy(x => x.Value.Name))
+            {
+                var eventObject = kvp.Value;
+
+                Console.WriteLine($"{eventObject.Name} - {eventObject.Participants.Count}");
+
+                foreach (var name in eventObject.Participants)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+
+        private static void AddParticipants(SortedDictionary<string, Event> eventsAndParticipants, string[] input, string currentId)
+        {
+            for (int i = 2; i < input.Length; i++)
+            {
+                eventsAndParticipants[currentId].Participants.Add(input[i]);
+            }
+        }
+    }
+}
