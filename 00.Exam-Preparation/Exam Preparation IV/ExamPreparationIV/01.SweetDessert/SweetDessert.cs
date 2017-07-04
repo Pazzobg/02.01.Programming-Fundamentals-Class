650c586fcc496a854d0bb4c4f07bf22566a412ae
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-04 19:27:40 +0300

    Added solutions of Exam Preparation IV (Pr.01 & Pr.02)

---
+++ 00.Exam-Preparation/Exam Preparation IV/ExamPreparationIV/01.SweetDessert/SweetDessert.cs
@@ -0,0 +1,42 @@
+﻿namespace _01.SweetDessert
+{
+    using System;
+
+    public class SweetDessert
+    {
+        public static void Main()
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            int numberOfGuests = int.Parse(Console.ReadLine());
+            decimal priceBananaPerPiece = decimal.Parse(Console.ReadLine());
+            decimal priceEggsPerPiece = decimal.Parse(Console.ReadLine());
+            decimal priceBerriesPerPiece = decimal.Parse(Console.ReadLine());
+
+            int countOfSetsToPrepare = numberOfGuests / 6;
+            if (numberOfGuests % 6 != 0)
+            {
+                countOfSetsToPrepare++;
+            }
+
+            int totalBananas = countOfSetsToPrepare * 2;
+            int totalEggs = countOfSetsToPrepare * 4;
+            decimal totalBerries = countOfSetsToPrepare * 0.2m;
+
+            decimal totalCosts =
+                (totalBananas * priceBananaPerPiece)
+                + (totalEggs * priceEggsPerPiece)
+                + (totalBerries * priceBerriesPerPiece);
+
+            var difference = totalCosts - budget;
+
+            if (difference <= 0)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {totalCosts:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {difference:f2}lv more.");
+            }
+        }
+    }
+}
---
+++ 00.Exam-Preparation/Exam Preparation IV/ExamPreparationIV/02.ArrayManipulator/ArrayManipulator.cs
@@ -0,0 +1,150 @@
+﻿namespace _02.ArrayManipulator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ArrayManipulator
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            while (input[0] != "end")
+            {
+                string command = input[0];
+                switch (command)
+                {
+                    case "exchange":
+                        ExchageArrayElements(numbers, input); break;
+                    case "max":
+                        CalculateIndexOfMaxEvenOddElement(numbers, input); break;
+                    case "min":
+                        CalculateIndexOfMinEvenOddElement(numbers, input); break;
+                    case "first":
+                        CalculateFirstEvenOddElements(numbers, input); break;
+                    case "last":
+                        CalculateLastEvenOddElements(numbers, input); break;
+                }
+
+                input = Console.ReadLine().Split(' ');
+            }
+
+            Console.WriteLine($"[{string.Join(", ", numbers)}]");
+        }
+
+        public static void ExchageArrayElements(int[] numbers, string[] input)
+        {
+            int index = int.Parse(input[1]);
+
+            if (index < 0 || index > numbers.Length - 1)
+            {
+                Console.WriteLine("Invalid index");
+                return;
+            }
+
+            int loopEnd = numbers.Length - 1 - index;
+            for (int i = 0; i < loopEnd; i++)
+            {
+                int temp = numbers[numbers.Length - 1];
+
+                for (int j = numbers.Length - 1; j > 0; j--)
+                {
+                    numbers[j] = numbers[j - 1];
+                }
+
+                numbers[0] = temp;
+            }
+        }
+
+        public static void CalculateIndexOfMaxEvenOddElement(int[] numbers, string[] input)
+        {
+            string oddOrEven = input[1];
+
+            int maxNum = -1;
+
+            if (oddOrEven == "even" && numbers.Any(x => x % 2 == 0))
+            {
+                maxNum = numbers.Where(x => x % 2 == 0).Max();
+            }
+            else if (oddOrEven == "odd" && numbers.Any(x => x % 2 != 0))
+            {
+                maxNum = numbers.Where(x => x % 2 != 0).Max();
+            }
+
+            Console.WriteLine(maxNum > -1 ? Array.LastIndexOf(numbers, maxNum).ToString() : "No matches");
+        }
+
+        public static void CalculateIndexOfMinEvenOddElement(int[] numbers, string[] input)
+        {
+            string oddOrEven = input[1];
+
+            int maxNum = -1;
+
+            if (oddOrEven == "even" && numbers.Any(x => x % 2 == 0))
+            {
+                maxNum = numbers.Where(x => x % 2 == 0).Min();
+            }
+            else if (oddOrEven == "odd" && numbers.Any(x => x % 2 != 0))
+            {
+                maxNum = numbers.Where(x => x % 2 != 0).Min();
+            }
+
+            Console.WriteLine(maxNum > -1 ? Array.LastIndexOf(numbers, maxNum).ToString() : "No matches");
+        }
+
+        public static void CalculateFirstEvenOddElements(int[] numbers, string[] input)
+        {
+            int count = int.Parse(input[1]);
+
+            if (count > numbers.Length)
+            {
+                Console.WriteLine("Invalid count");
+                return;
+            }
+
+            string oddOrEven = input[2];
+
+            var output = new List<int>();
+
+            if (oddOrEven == "even")
+            {
+                output = numbers.Where(x => x % 2 == 0).Take(count).ToList();
+            }
+            else if (oddOrEven == "odd")
+            {
+                output = numbers.Where(x => x % 2 != 0).Take(count).ToList();
+            }
+
+            Console.WriteLine($"[{string.Join(", ", output)}]");
+        }
+
+        public static void CalculateLastEvenOddElements(int[] numbers, string[] input)
+        {
+            int count = int.Parse(input[1]);
+
+            if (count > numbers.Length)
+            {
+                Console.WriteLine("Invalid count");
+                return;
+            }
+
+            string oddOrEven = input[2];
+
+            var output = new List<int>();
+
+            if (oddOrEven == "even")
+            {
+                output = numbers.Where(x => x % 2 == 0).Reverse().Take(count).Reverse().ToList();
+            }
+            else if (oddOrEven == "odd")
+            {
+                output = numbers.Where(x => x % 2 != 0).Reverse().Take(count).Reverse().ToList();
+            }
+
+            Console.WriteLine($"[{string.Join(", ", output)}]");
+        }
+    }
+}
