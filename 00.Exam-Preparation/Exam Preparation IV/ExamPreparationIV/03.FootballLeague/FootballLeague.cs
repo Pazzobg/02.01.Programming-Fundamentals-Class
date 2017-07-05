37a7b7decea6d3a0d63c177fe0fd9f008ceb173f
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-05 11:14:15 +0300

    Added solutions of Exam Preparation IV (Pr.03 & Pr.04)

---
+++ 00.Exam-Preparation/Exam Preparation IV/ExamPreparationIV/03.FootballLeague/FootballLeague.cs
@@ -0,0 +1,106 @@
+﻿namespace _03.FootballLeague
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class FootballLeague
+    {
+        public static void Main()
+        {
+            var scoresTable = new Dictionary<string, int>();
+            var goalsTable = new SortedDictionary<string, long>();
+
+            string delimiter = Console.ReadLine();
+            delimiter = Regex.Escape(delimiter);
+            string input = Console.ReadLine();
+
+            while (input != "final")
+            {
+                string pattern = $@".*?(?:{delimiter})(.*?)(?:{delimiter}).*?(?:{
+                    delimiter})(.*?)(?:{delimiter}).*?(\d+):(\d+)";
+                var regex = new Regex(pattern);
+
+                string first = regex.Match(input).Groups[1].Value.ToUpper();
+                string firstTeamName = GetTeamName(first);  // one option to get it
+                string second = regex.Match(input).Groups[2].Value.ToUpper();  // another option to get it
+                var secondName = second.ToCharArray().Reverse().ToArray();
+                string secondTeamName = new string(secondName);
+
+                int firstTeamGoals = int.Parse(regex.Match(input).Groups[3].Value);
+                int secondTeamGoals = int.Parse(regex.Match(input).Groups[4].Value);
+
+                int firstTeamScore = 0;
+                int secondTeamScore = 0;
+
+                if (firstTeamGoals > secondTeamGoals)
+                {
+                    firstTeamScore = 3;
+                }
+                else if (secondTeamGoals > firstTeamGoals)
+                {
+                    secondTeamScore = 3;
+                }
+                else
+                {
+                    firstTeamScore = 1;
+                    secondTeamScore = 1;
+                }
+
+                if (!scoresTable.ContainsKey(firstTeamName))
+                {
+                    scoresTable[firstTeamName] = 0;
+                }
+                scoresTable[firstTeamName] += firstTeamScore;
+
+                if (!goalsTable.ContainsKey(firstTeamName))
+                {
+                    goalsTable[firstTeamName] = 0;
+                }
+                goalsTable[firstTeamName] += firstTeamGoals;
+
+                if (!scoresTable.ContainsKey(secondTeamName))
+                {
+                    scoresTable[secondTeamName] = 0;
+                }
+                scoresTable[secondTeamName] += secondTeamScore;
+
+                if (!goalsTable.ContainsKey(secondTeamName))
+                {
+                    goalsTable[secondTeamName] = 0;
+                }
+                goalsTable[secondTeamName] += secondTeamGoals;
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("League standings:");
+            int j = 1;
+            foreach (var kvp in scoresTable.OrderByDescending(x => x.Value).ThenBy(t => t.Key))
+            {
+                Console.WriteLine($"{j}. {kvp.Key} {kvp.Value}");
+                j++;
+            }
+
+            Console.WriteLine("Top 3 scored goals:");
+            foreach (var kvp in goalsTable.OrderByDescending(x => x.Value).Take(3))
+            {
+                Console.WriteLine($"- {kvp.Key} -> {kvp.Value}");
+            }
+        }
+
+        public static string GetTeamName(string team)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = team.Length - 1; i >= 0; i--)
+            {
+                sb.Append(team[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 00.Exam-Preparation/Exam Preparation IV/ExamPreparationIV/04.CubicMessages/CubicMessages.cs
@@ -0,0 +1,64 @@
+﻿namespace _04.CubicMessages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class CubicMessages
+    {
+        public static void Main()
+        {
+            var messagesFound = new List<string>();
+            string messageLine = Console.ReadLine();
+
+            while (messageLine != "Over!")
+            {
+                int exactLength = int.Parse(Console.ReadLine());
+
+                string pattern = $@"^(\d+)([a-zA-Z]{{{exactLength}}})([^a-zA-Z]*?)$";
+                var regex = new Regex(pattern);
+
+                var match = regex.Match(messageLine);
+
+                if (match.Success)
+                {
+                    messagesFound.Add(messageLine);
+                }
+
+                messageLine = Console.ReadLine();
+            }
+
+            for (int i = 0; i < messagesFound.Count; i++)
+            {
+                string currentMessage = messagesFound[i];
+
+                string pattern = $@"^(\d+)([a-zA-Z]+)([^a-zA-Z]*?)$";
+                var regex = new Regex(pattern);
+
+                string messageText = regex.Match(currentMessage).Groups[2].Value;
+                string digitsFront = regex.Match(currentMessage).Groups[1].Value;
+                string symbolsBack = regex.Match(currentMessage).Groups[3].Value;
+
+                regex = new Regex(@"\d");
+                var indexes = new List<int>();
+                indexes = regex.Matches(digitsFront).Cast<Match>().Select(x => x.Value).Select(int.Parse).ToList();
+                indexes.AddRange(regex.Matches(symbolsBack).Cast<Match>().Select(x => x.Value).Select(int.Parse).ToList());
+
+                var sb = new StringBuilder();
+
+                for (int j = 0; j < indexes.Count; j++)
+                {
+                    if (indexes[j] >= 0 && indexes[j] < messageText.Length)
+                    {
+                        sb.Append(messageText[indexes[j]]);
+                    }
+                    else sb.Append(" ");
+                }
+
+                Console.WriteLine($"{messageText} == {sb.ToString()}");
+            }
+        }
+    }
+}
