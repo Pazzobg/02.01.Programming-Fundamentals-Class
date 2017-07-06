4ac55448d21ffc813f28bfb2b834876b45ff6dc0
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-06 18:54:11 +0300

    Added solutions of Pr.02 & Pr.04 of Programming Fundamentals Sample Exam I JUN16

---
+++ 00.Exam-Preparation/08. PF Sample Exam I JUN16/Sample Exam I JUN16/02.ArrayModifier/Arraymodifier.cs
@@ -0,0 +1,56 @@
+﻿namespace _02.ArrayModifier
+{
+    using System;
+    using System.Linq;
+
+    public class Arraymodifier
+    {
+        public static void Main()
+        {
+            long[] numbers = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            while (input[0] != "end")
+            {
+                string command = input[0];
+
+                switch (command)
+                {
+                    case "swap": SwapTwoElements(numbers, input[1], input[2]); break;
+                    case "multiply": MultiplyTwoElements(numbers, input[1], input[2]); break;
+                    case "decrease": DecreaseValuesOfAllElements(numbers); break;
+                }
+
+                input = Console.ReadLine().Split(' ');
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        public static void DecreaseValuesOfAllElements(long[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i]--;
+            }
+        }
+
+        public static void MultiplyTwoElements(long[] numbers, string v1, string v2)
+        {
+            int index1 = int.Parse(v1);
+            int index2 = int.Parse(v2);
+            long result = numbers[index1] * numbers[index2];
+            numbers[index1] = result;
+        }
+
+        public static void SwapTwoElements(long[] numbers, string v1, string v2)
+        {
+            int index1 = int.Parse(v1);
+            int index2 = int.Parse(v2);
+            long temp = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = temp;
+        }
+    }
+}
---
+++ 00.Exam-Preparation/08. PF Sample Exam I JUN16/Sample Exam I JUN16/04.PopulationAggregation/PopulationAggregation.cs
@@ -0,0 +1,87 @@
+﻿namespace _04.PopulationAggregation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class PopulationAggregation
+    {
+        public static void Main()
+        {
+            var countriesCitiesCounts = new SortedDictionary<string, List<string>>();
+            var citiesPopulations = new Dictionary<string, long>();
+
+            string line = Console.ReadLine();
+
+            while (line != "stop")
+            {
+                string[] input = line.Split('\\');
+
+                string countryToken = string.Empty;
+                string cityToken = string.Empty;
+                long population = long.Parse(input[2]);
+
+                char firstChar = input[0][0];
+                if (char.IsUpper(firstChar))
+                {
+                    countryToken = input[0];
+                    cityToken = input[1];
+                }
+                else
+                {
+                    countryToken = input[1];
+                    cityToken = input[0];
+                }
+
+                string country = RemoveProhibitedSymbolsFromName(countryToken);
+                string city = RemoveProhibitedSymbolsFromName(cityToken);
+                
+                // add country-city pair to dict.
+                if (!countriesCitiesCounts.ContainsKey(country))     
+                {
+                    countriesCitiesCounts[country] = new List<string>();
+                }
+
+                countriesCitiesCounts[country].Add(city);
+
+                // add city-population pair to another dict.
+                if (!citiesPopulations.ContainsKey(city))           
+                {
+                    citiesPopulations[city] = 0;
+                }
+
+                citiesPopulations[city] = population;
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var kvp in countriesCitiesCounts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value.Count}");
+            }
+
+            foreach (var kvp in citiesPopulations.OrderByDescending(x => x.Value).Take(3))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+
+        public static string RemoveProhibitedSymbolsFromName(string token)
+        {
+            string patternSplit = @"[0-9@#\$&]";
+
+            string[] resultAsCharArr = Regex.Split(token, patternSplit);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < resultAsCharArr.Length; i++)
+            {
+                sb.Append(resultAsCharArr[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
