41b5d51265b86feea3c145e190935da9956937f0
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-06 16:58:19 +0300

    Added solutions of Pr.01 & Pr.02 of Programming Fundamentals Exam 12JUN16

---
+++ 00.Exam-Preparation/07. PF Exam 12JUN16/Exam12JUN16/01.Numbers/Numbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class Numbers
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var output = numbers
+                .Where(x => x > numbers.Average())
+                .OrderByDescending(x => x)
+                .Take(5)
+                .ToArray();
+
+            Console.WriteLine(output.Length > 0 ? string.Join(" ", output) : "No");
+        }
+    }
+}
---
+++ 00.Exam-Preparation/07. PF Exam 12JUN16/Exam12JUN16/02.SoftUniCoffeeOrders/SoftUniCoffeeOrders.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.SoftUniCoffeeOrders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SoftUniCoffeeOrders
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal totalPrice = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+                var orderDate = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                long capsulesCount = long.Parse(Console.ReadLine());
+
+                int daysInMonth = DateTime.DaysInMonth(orderDate.Year, orderDate.Month);
+
+                decimal currentPrice = (daysInMonth * capsulesCount) * pricePerCapsule;
+
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+                totalPrice += currentPrice;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
