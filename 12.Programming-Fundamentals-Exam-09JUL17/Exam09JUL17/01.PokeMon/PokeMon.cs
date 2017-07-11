db611a446017a9431e0357948d52cb46a91abc25
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-07-11 08:32:26 +0300

    Added problems & solutions of my actual Programming Fundamentals Exam 09JUL17

---
+++ 12.Programming-Fundamentals-Exam-09JUL17/Exam09JUL17/01.PokeMon/PokeMon.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.PokeMon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PokeMon
+    {
+        public static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            decimal halfPower = pokePower * 0.5m;
+
+            while (pokePower >= distanceBetweenTargets)
+            {
+                pokePower -= distanceBetweenTargets;
+                count++;
+
+                if (pokePower == halfPower && exhaustionFactor > 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(count);
+        }
+    }
+}
